0a083c0b78714cfdea59e479ccb07958a2bdcb2d
meaden164 <103168732+meaden164@users.noreply.github.com>
2022-04-18 10:49:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,86 @@
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+// Определение устройства
+"use strict"
+
+
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android() || 
+                isMobile.BlackBerry() || 
+                isMobile.iOS() || 
+                isMobile.Opera() || 
+                isMobile.Windows());
+    }
+};
+
+// Выпадающее меню
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+    
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function (e){
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+} else {
+    document.body.classList.add('_pc');
+}
+
+// Плавная прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click",onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const mwnuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(mwnuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+        }e.preventDefault();
+    }
+}
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu__body');
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
 	});
-});
-$('.maths__item,.informatics__item,.physics__item').on('click', function () {
-	window.location.href = $(this).attr('url');
-});
+}
